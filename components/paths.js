2ca716b7b45438c05ef2ce5f5b9e038dce67b3fe
KaMeHb-UA <marlock@etlgr.com>
2020-05-28 18:34:23 +0300

    remove useless paths

--- components/paths.js
+++ components/paths.js
@@ -9,12 +9,8 @@ export const components = root + '/components'
 
 export const assets = root + '/assets'
 
-export const constants = root + '/constants'
-
 export const containers = root + '/containers'
 
 export const css = assets + '/css'
 
 export const images = assets + '/images'
-
-export const pages = root + '/pages'
