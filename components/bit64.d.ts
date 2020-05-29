551bfd9adbc2914d4e2fa0a2b5958b707c125d5c
KaMeHb-UA <marlock@etlgr.com>
2020-05-30 00:30:09 +0300

    typings

---
+++ components/bit64.d.ts
@@ -0,0 +1,7 @@
+export function and(a: number, b: number): number
+
+export function or(a: number, b: number): number
+
+export function not(a: number): number
+
+export function xor(a: number, b: number): number
