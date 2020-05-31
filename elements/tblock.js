f437ebad8962a59658244cef13144c8f24938104
KaMeHb-UA <marlock@etlgr.com>
2020-05-31 11:42:14 +0300

    mapChildren already declared in block.js

--- elements/tblock.js
+++ elements/tblock.js
@@ -1,9 +1,5 @@
 import html, { Component } from '../components/preact.js'
-import parseElement from '../components/generator.js'
-
-export function mapChildren(page, childs, api){
-    return (childs || []).map(parseElement.bind(page, api))
-}
+import { mapChildren } from './block.js'
 
 export default class TBlock extends Component{
     render(){
