dcfb0f78816b6630a8e80c1dd167ca429ef67d6d
KaMeHb-UA <marlock@etlgr.com>
2020-05-31 11:03:36 +0300

    tblock

---
+++ elements/tblock.js
@@ -0,0 +1,15 @@
+import html, { Component } from '../components/preact.js'
+import parseElement from '../components/generator.js'
+
+export function mapChildren(page, childs, api){
+    return (childs || []).map(parseElement.bind(page, api))
+}
+
+export default class TBlock extends Component{
+    render(){
+        const { childs, api, page } = this.props;
+        return html`<div>${
+            mapChildren(page, childs, api)
+        }</div>`
+    }
+}
