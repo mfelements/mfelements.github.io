37a5f769f116f6d335f4ca6c5d1f8f2126216d15
KaMeHb <marlock@etlgr.com>
2020-06-02 12:20:52 +0300

    iOS support for 64bit bitwise operations

---
+++ components/bit64.d.ts
@@ -0,0 +1,7 @@
+export function and(a: number | bigint, b: number | bigint): number | bigint
+
+export function or(a: number | bigint, b: number | bigint): number | bigint
+
+export function not(a: number | bigint): number | bigint
+
+export function xor(a: number | bigint, b: number | bigint): number | bigint
