45fe2b91a39ede17f4e71079ca2d96a52e5b7ee9
KaMeHb <marlock@etlgr.com>
2020-07-29 15:26:38 +0300

    bitwise operations now produces regular 64-bit number

--- components/bit64.d.ts
+++ components/bit64.d.ts
@@ -1,7 +1,7 @@
-export function and(a: number | bigint, b: number | bigint): number | bigint
+export function and(a: number | bigint, b: number | bigint): number
 
-export function or(a: number | bigint, b: number | bigint): number | bigint
+export function or(a: number | bigint, b: number | bigint): number
 
-export function not(a: number | bigint): number | bigint
+export function not(a: number | bigint): number
 
-export function xor(a: number | bigint, b: number | bigint): number | bigint
+export function xor(a: number | bigint, b: number | bigint): number
--- components/bit64.js
+++ components/bit64.js
@@ -13,27 +13,27 @@ function pad(bit) {
 }
 
 export function and(a, b){
-    if(bigIntSupport) return BigInt(a) & BigInt(b);
+    if(bigIntSupport) return +(BigInt(a) & BigInt(b));
     const _a = divide(a);
     const _b = divide(b);
     return parseInt(pad((_a[0] & _b[0]) >>> 0) + pad((_a[1] & _b[1]) >>> 0), 2)
 }
 
 export function or(a, b){
-    if(bigIntSupport) return BigInt(a) | BigInt(b);
+    if(bigIntSupport) return +(BigInt(a) | BigInt(b));
     const _a = divide(a);
     const _b = divide(b);
     return parseInt(pad((_a[0] | _b[0]) >>> 0) + pad((_a[1] | _b[1]) >>> 0), 2)
 }
 
 export function not(a){
-    if(bigIntSupport) return ~BigInt(a);
+    if(bigIntSupport) return +(~BigInt(a));
     const _a = divide(a);
     return parseInt(pad((~_a[0]) >>> 0) + pad((~_a[1]) >>> 0), 2)
 }
 
 export function xor(a, b){
-    if(bigIntSupport) return BigInt(a) ^ BigInt(b);
+    if(bigIntSupport) return +(BigInt(a) ^ BigInt(b));
     const _a = divide(a);
     const _b = divide(b);
     return parseInt(pad((_a[0] ^ _b[0]) >>> 0) + pad((_a[1] ^ _b[1]) >>> 0), 2)
