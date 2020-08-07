8325e891ed169c7408e6dbdb2115eda4da1ba2b8
KaMeHb-UA <marlock@etlgr.com>
2020-08-07 07:31:34 +0300

    initial dialog, close #6
    Co-Authored-By: FallHouse <fallhouse@users.noreply.github.com>

---
+++ elements/dialog.js
@@ -0,0 +1,29 @@
+import html, { Component } from '../components/preact.js'
+import Body from '../containers/body.js'
+import Button from './button.js'
+
+export default class Dialog extends Component{
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+    }
+    render(){
+        const { text, btnText, buttons, api, page } = this.props;
+        const { open } = this.state;
+        return html`
+            <button onClick=${() => this.setState({ open: true })}>${btnText}</>
+            <${Body}>
+                <dialog open=${open}>
+                    <div>${text}</>
+                    <div class=buttons>${(buttons || []).map(btnProps => html`
+                        <span onclick=${() => this.setState({ open: false })}>
+                            <${Button} ...${btnProps} ...${{ api, page }}/>
+                        </>
+                    `)}</>
+                </>
+                <div class=${'dialog-bg'} onclick=${() => this.setState({ open: false })}/>
+            </>
+        `
+    }
+}
