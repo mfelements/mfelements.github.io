b81a4596508717263d0987eee522b448a05fcc81
KaMeHb-UA <marlock@etlgr.com>
2020-09-17 20:27:22 +0300

    system button as dialog opener

--- elements/dialog.js
+++ elements/dialog.js
@@ -1,13 +1,14 @@
 import html, { Component } from '../components/preact.js'
 import Body from '../containers/body.js'
 import Button from './button.js'
+import SystemButton from '../systemElements/button.js'
 
 export default class Dialog extends Component{
     render(){
         const { text, btnText, buttons, api, page } = this.props;
         const { open } = this.state;
         return html`
-            <button onClick=${() => this.setState({ open: true })}>${btnText}</>
+            <${SystemButton} onclick=${() => this.setState({ open: true })}>${btnText}</>
             <${Body}>
                 <dialog open=${open}>
                     <div>${text}</>
