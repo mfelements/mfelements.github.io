81ef15e915979feb7357b7f58a561977809a1ebd
KaMeHb <marlock@etlgr.com>
2020-09-21 15:37:13 +0300

    no more needed

--- components/media.d.ts
+++
@@ -1,9 +0,0 @@
-import { Unpromisify } from '../@typings/helpers'
-
-export function supports(type: string): boolean
-
-export function supportsHLSNatively(): boolean
-
-export function isHLS(url: string): Promise<[ string, boolean ]>
-
-export function splitListByHLS(list: Unpromisify<ReturnType<typeof isHLS>>[]): [ string[], string[] ]
