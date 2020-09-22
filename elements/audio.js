7311d5c69a1ca05386c2100a090e958f67e59346
KaMeHb-UA <marlock@bassteam.ml>
2020-09-22 23:09:40 +0300

    hls support

--- elements/audio.js
+++ elements/audio.js
@@ -1,13 +1,25 @@
 import html, { Component } from '../components/preact.js'
 import Plyr from '../components/plyr.js'
+import { isHls } from '../components/media.js'
+import hls from '../components/hls.js'
+
+function getHls(list){
+    for(const { type, src } of list) if(isHls(type)) return src
+}
 
 export default class Audio extends Component{
     constructor(props){
         super(props);
+        /** @type {{ Hls: import('../@typings/helpers').Unpromisify<hls> }} */
+        this.state = {};
         this.options = {}
     }
     async componentDidMount(){
         if(!this.audio) this.audio = this.base.children[0];
+        hls.then(Hls => {
+            this.hlsSupport = Hls.isSupported();
+            this.setState({ Hls })
+        });
         this.player = new (await Plyr)(this.audio, this.options);
     }
     componentWillUnmount(){
@@ -15,10 +27,27 @@ export default class Audio extends Component{
         this.player = null
     }
     render(){
-        const { sources } = this.props;
+        const { props: { sources }, state: { Hls }, hlsSupport, hlsInstance } = this;
+        const hls = getHls(sources || []);
+        if(hls){
+            if(!Hls) return null;
+            if(hlsSupport) return html`<div class=md-audio>
+                <audio controls>
+                    <source type=audio/mpegurl src=${hls}/>
+                </>
+            </>`;
+            if(!hlsInstance) setTimeout(() => {
+                if(this.hlsInstance) return;
+                const hlsInstance = new Hls();
+                this.hlsInstance = hlsInstance;
+                hlsInstance.loadSource(hls);
+                hlsInstance.attachMedia(this.audio)
+            });
+            return html`<div class=md-audio><audio controls/></>`
+        }
         return html`<div class=md-audio>
-            <audio>${(sources || []).map(({ type, src }) => html`
-                <source ...${{ type, src }}/>
+            <audio controls>${(sources || []).map(({ type, src }) => html`
+                <source type=${type || ''} src=${src || ''}/>
             `)}</>
         </>`
     }
