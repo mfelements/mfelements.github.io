7484983d59d4a71bafe26d6c500fdea4a2d22089
KaMeHb-UA <marlock@bassteam.ml>
2020-10-21 22:21:46 +0300

    button as typed component

--- elements/button.js
+++ elements/button.js
@@ -1,9 +1,13 @@
-import html, { Component } from '../components/preact.js'
+import html from '../components/preact.js'
 import { editables } from './editable.js'
 import parseElement from '../components/generator.js'
 import SystemButton from '../systemElements/button.js'
+import TypedComponent, { propsTypesSymbol } from '../components/typedComponent.js'
 
-export default class Button extends Component{
+export default class Button extends TypedComponent{
+    static get [propsTypesSymbol](){
+        return {}
+    }
     async click(){
         const { onClick, api, page } = this.props;
         const res = onClick && onClick.action ? await api[onClick.action](...(onClick.args || [])) : {};
